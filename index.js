262062c4abaef0c560d7d08374ef3cdf4e367df1
Rayndrick <132782407+Rayndrick@users.noreply.github.com>
2026-02-24 23:19:11 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+
+const token = process.env.TOKEN;
+const bot = new TelegramBot(token);
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
+
+    if (text && text.toLowerCase().includes("gastei")) {
+        bot.sendMessage(chatId, "Despesa registrada ✅");
+    } else {
+        bot.sendMessage(chatId, "Envie algo como: Gastei 200 supermercado");
+    }
+});
+
+const app = express();
+app.get("/", (req, res) => res.send("Bot rodando"));
+app.listen(3000);
